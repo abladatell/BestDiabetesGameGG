c63f9ec401f5ec0c2216655a4b1dddf015ee7cc6
Jake Lim <abladatell@gmail.com>
2019-04-25 15:25:48 -0700

    .

---
+++ WebDev/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>Diabetes Game</h1>
+      <!-- <Home></Home> -->
+  </div>
+</template>
+
+<script>
+// import Home from "./components/Home.vue"
+// export default {
+//     components: {
+//         home: Home
+//     }
+// };
+</script>
+
+<style>
+@font-face {
+    font-family: "SEGA LOGO FONT";
+    font-style: normal;
+    font-weight: normal;
+    src: local("SEGA LOGO FONT"), url("SEGA.woff") format("woff");
+}
+
+h1 {
+    font-family: "SEGA LOGO FONT";
+    font-weight: 200;
+    color: #0ccac4;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  background: linear-gradient(to left, rgba(7,27,82,1) 0%, rgba(0,128,128,1) 100%); /* w3c */
+}
+</style>
