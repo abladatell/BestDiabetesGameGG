1381289a0dcd4e484ebe1c9aa395ca958ce3af88
Jake Lim <abladatell@gmail.com>
2019-05-01 03:59:18 -0700

    feature added in navbar

--- web-dev/src/components/NavBar.vue
+++ web-dev/src/components/NavBar.vue
@@ -2,14 +2,13 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="info" sticky>
       <!-- <b-navbar-brand href>Title</b-navbar-brand> -->
-
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
           <b-nav-item to="/" active-class="active" exact class="items" id="home">Home</b-nav-item>
           <b-nav-item to="/link1" active-class="active" class="items">Link</b-nav-item>
-          <b-nav-item href="/game" class="items">Link3</b-nav-item>
+          <b-nav-item href="/game" active-class="active" class="items" :class="{active: isActive()}">Game</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
@@ -18,10 +17,10 @@
             <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
             <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
           </b-nav-form>
-
+          
           <b-nav-item-dropdown text="Mode" right>
             <b-dropdown-item href="#">Hard</b-dropdown-item>
-            <b-dropdown-item href="#">Insane</b-dropdown-item>
+            <b-dropd2own-item href="#">Insane</b-dropd2own-item>
           </b-nav-item-dropdown>
 
           <b-nav-item-dropdown right>
@@ -39,18 +38,28 @@
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            isActive() {
+                if (this.$route.path === "/game")
+                    return true;
+                return false;
+            }
+        }
+    }
+};
 </script>
 
 
 <style scoped>
 .items {
-    margin-right: 5px;
-    margin-left: 5px;
+  margin-right: 5px;
+  margin-left: 5px;
 }
 
 #home {
-    margin-left: 50px;
+  margin-left: 50px;
 }
 
 .active {
@@ -58,7 +67,6 @@ export default {};
   background-color: #0062cc;
   border-color: #005cbf;
   box-shadow: 0 0 0 0.2rem rgba(0, 123, 255, 0.5);
-  padding: 0.5rem 1rem;
   border-radius: 0.3rem;
 }
 </style>
--- web-dev/src/main.js
+++ web-dev/src/main.js
@@ -13,13 +13,13 @@ Vue.use(BootstrapVue);
 Vue.config.productionTip = false;
 
 const router = new VueRouter({
-  routes,
-  mode: "history"
+    routes,
+    mode: "history"
 });
 
 
 new Vue({
-  el: "#app",
-  router,
-  render: h => h(App),
+    el: "#app",
+    router,
+    render: h => h(App),
 })
