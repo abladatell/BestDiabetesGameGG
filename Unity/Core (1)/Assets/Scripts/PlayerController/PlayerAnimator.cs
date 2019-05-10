cf0a5273b28a769dbdafeb69767b6ed86f704ee5
Garrett Tailby <lastgameforlife@hotmail.com>
2019-05-09 22:36:52 -0700

    Added Chase AI

---
+++ Unity/Core (1)/Assets/Scripts/PlayerController/PlayerAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    Animator animator;
+    AnimatorOverrideController overrideController;
+
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        overrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
+        animator.runtimeAnimatorController = overrideController;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnAttack()
+    {
+        animator.SetTrigger("attack");
+    }
+}
--- Unity/Core (1)/Assets/Scripts/ProjectileScripts/ProjectileSpawner.cs
+++ Unity/Core (1)/Assets/Scripts/ProjectileScripts/ProjectileSpawner.cs
@@ -12,9 +12,9 @@ public class ProjectileSpawner : MonoBehaviour
     public float shootRate = 0f;
     public float shootForce = 0f;
     private float timeElapsed = 0f;
-    public float accuracy = 0f;
     public bool isPlayer = false;
     public bool isAggro = false;
+    private Vector3 lookPos;
 
     // Start is called before the first frame update
     void Start()
@@ -28,8 +28,23 @@ public class ProjectileSpawner : MonoBehaviour
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
+        
 
         rbody.velocity = new Vector3(h, v, 0);
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            lookPos = hit.point;
+        }
+
+        Vector3 lookDir = lookPos - transform.position;
+        lookDir.y = 0;
+
+        transform.LookAt(transform.position + lookDir, Vector3.up);
 
         if (isPlayer && (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0)) || !isPlayer && isAggro) {
             if(timeElapsed > shootRate)
